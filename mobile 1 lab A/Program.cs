bea8b71efe66e38194842277964532542492a4a9
tbreier25 <63306086+tbreier25@users.noreply.github.com>
2020-04-27 15:26:38 -0500

    Add files via upload

---
+++ mobile 1 lab A/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mobile_1_lab_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int age = 21;
+            
+
+            Console.WriteLine("Enter your age : {0}", x);
+            if ((x>20) != 0)
+            {
+                Console.WriteLine("You are of legal age to drink");
+            }
+            else
+            {
+                Console.WriteLine("You are too young to drink");
+            }
+           
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
